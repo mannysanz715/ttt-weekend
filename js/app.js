5417de5031290b63f040d5949acd8a95343ee1c8
mannysanz715 <mannysanz715@gmail.com>
2022-12-09 13:44:44 -0800

    Add game logic

--- js/app.js
+++ js/app.js
@@ -1,133 +1,211 @@
 /*-------------------------------- Constants --------------------------------*/
-
+const winningCombos = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
 
 
 /*---------------------------- Variables (state) ----------------------------*/
 
-  let board, turn, winner, tie
+  let board, turn, winner, tie = false
 
 
 
 /*------------------------ Cached Element References ------------------------*/
 
-  const squareEls = 0
+  const squareEls = [document.getElementById('sq0'), document.getElementById('sq1'),document.getElementById('sq2'),document.getElementById('sq3'),document.getElementById('sq4'),document.getElementById('sq5'),document.getElementById('sq6'),document.getElementById('sq7'),document.getElementById('sq8')]
 
-/*----------------------------- Event Listeners -----------------------------*/
+  const messageEl = document.getElementById('message')
 
+  const boardEl = document.querySelector('.board')
 
+/*----------------------------- Event Listeners= -----------------------------*/
+
+  boardEl.addEventListener('click',handleClick)
 
 /*-------------------------------- Functions --------------------------------*/
 
-// Step 1 - Define the required variables used to track the state of the game
 
-  // 1a) Use a variable named `board` to represent the state of the squares on
-  //     the board.
+init()
 
-  // 1b) Use a variable named `turn` to track whose turn it is.
+function init(){
+  board = [1, null, null, null, null, null, null, null, null]
+  turn = 1
+  winner = false
+  tie = false
+  render()
+}
 
-  // 1c) Use a variable named `winner` to represent if anyone has won yet.
+function render(){
+  updateBoard()
+  updateMessage()
 
-  // 1d) Use a variable named `tie` to represent if the game has ended in a tie.
+}
 
+function updateBoard(){
+  board.forEach(function(tile, idx){
+    if(tile === 1){
+      squareEls[idx].style.backgroundColor = 'lightblue'
+    }
+    else if(tile === -1){
+      squareEls[idx].style.backgroundColor = 'lightcoral'
+    }
+  })
+}
+function updateMessage(){
+  if(winner === false && tie === false){
+    messageEl.textContent = `It is ${turn}'s turn`
+  } else if (winner === false && tie === true){
+    messageEl.textContent = 'Game is a tie'
+  }else{
+    messageEl.textContent = `Congratulations ${turn}`
+  }
+}
 
-// Step 2 - Store cached element references.
+function handleClick(evt){
+  const sqIdx = evt.target.id.at(-1)
+  if(board[sqIdx]){
+    return
+  }else if (winner != false){
+    return
+  }
+  placePiece()
+}
 
-  // 2a) In a constant called `squareEls`, store the nine elements 
-  //    representing the squares on the page.
+function placePiece(idx){
+  board[idx] = turn
+}
 
-  // 2b) In a constant called `messageEl`, store the element that displays the 
-  //    game's status on the page.
+function checkForTie(){
+  if(board.some(null)){
+    return
+  }else{
+    tie = false
+  }
+}
 
-
-// Step 3 - Upon loading, the game state should be initialized, and a function 
-//          should be called to render this game state.
-
-  // 3a) Create a function called `init`.
-
-  // 3b) Call this `init` function when the app loads.
+function checkForWinner(){
   
-  // 3c) Set the `board` variable to an array containing nine `null`s to 
-  //    represent empty squares.
-
-  // 3d) Set the `turn` to `1` - which will represent player X.
-
-  // 3e) Set the `winner` to false.
-
-  // 3f) Set `tie` to false.
-
-  // 3g) Call a function called `render` at the end of the `init` function.
+}
 
 
-// Step 4 - The state of the game should be rendered to the user
 
-  // 4a) Create a function called `render`, then set it aside for now.
 
-  // 4b) Create a function called `updateBoard`.
+//todo Step 1 - Define the required variables used to track the state of the game
 
-  // 4c) In the `updateBoard` function, loop over `board` and for each element:
-  //     - Use the current index of the iteration to access the corresponding 
-  //       square in the `squareEls` array.
-  //     - Style that square however you wish, dependent on the value  
-  //       contained in the current cell being iterated over (`-1`, `1`, or
-  //       `null`). To keep it simple, start with just putting a letter in 
-  //       each square depending on what the the value of each cell is.
+  //// 1a) Use a variable named `board` to represent the state of the squares on
+  // //    the board.
 
-  // 4d) Create a function called `updateMessage`
+  //// 1b) Use a variable named `turn` to track whose turn it is.
+
+  //// 1c) Use a variable named `winner` to represent if anyone has won yet.
+
+  //// 1d) Use a variable named `tie` to represent if the game has ended in a tie.
+
+
+//todo Step 2 - Store cached element references.
+
+  ////  2a) In a constant called `squareEls`, store the nine elements 
+  ////    representing the squares on the page.
+
+  //// 2b) In a constant called `messageEl`, store the element that displays the 
+  ////  game's status on the page.
+
+
+//todo Step 3 - Upon loading, the game state should be initialized, and a function should be called to render this game state.
+
+  //// 3a) Create a function called `init`.
+
+  //// 3b) Call this `init` function when the app loads.
   
-  // 4e) In the `updateMessage` function, render a message based on the 
-  //     current game state:
-  //     - If both `winner` and `tie` have a value of false (meaning the game 
-  //       is still in progress), render whose turn it is.
-  //     - If `winner` is false, but `tie` is true, render a tie message.
-  //     - Otherwise, render a congratulatory message to the player that has 
-  //       won.
+  //// 3c) Set the `board` variable to an array containing nine `null`s to 
+  ////    represent empty squares.
 
-  // 4f) Invoke both the `updateBoard` and the `updateMessage` functions
-  //     inside of your `render` function.
+  //// 3d) Set the `turn` to `1` - which will represent player X.
 
-// Step 5 - Define the required constants
+  //// 3e) Set the `winner` to false.
 
-  // 5a) In a constant called `winningCombos` define the eight possible winning 
-  //     combinations as an array of arrays.
+  //// 3f) Set `tie` to false.
+
+  //// 3g) Call a function called `render` at the end of the `init` function.
 
 
-// Step 6 - Handle a player clicking a square with a `handleClick` function
+//todo Step 4 - The state of the game should be rendered to the user
 
-  // 6a) Create a function called `handleClick`. It will have an `evt`
-  //     parameter.
+ // // 4a) Create a function called `render`, then set it aside for now.
 
-  // 6b) Attach an event listener to the game board (you can do this to each
-  //     one of the existing `squareEls` with a `forEach` loop OR add a new
-  //     cached element reference that will allow you to take advantage of 
-  //     event bubbling). On the `'click'` event, it should call the 
-  //    `handleClick` function you created in 6a.
+  ////  4b) Create a function called `updateBoard`.
 
-  // 6c) Obtain the index of the square that was clicked by "extracting" the 
-  //     index from an `id` assigned to the target element in the HTML. Assign 
-  //     this to a constant called `sqIdx`.
+  //// 4c) In the `updateBoard` function, loop over `board` and for each element:
+  ////     - Use the current index of the iteration to access the corresponding 
+  ////       square in the `squareEls` array.
+  //  //   - Style that square however you wish, dependent on the value  
+  //    //   contained in the current cell being iterated over (`-1`, `1`, or
+  ////       `null`). To keep it simple, start with just putting a letter in 
+  ////       each square depending on what the the value of each cell is.
 
-  // 6d) If the `board` has a value at the `sqIdx`, immediately `return`  
-  //     because that square is already taken. Also, if `winner` is not `null`
-  //     immediately `return` because the game is over.
+  //// 4d) Create a function called `updateMessage`
+  
+  //// 4e) In the `updateMessage` function, render a message based on the 
+  ////     current game state:
+  //  //   - If both `winner` and `tie` have a value of false (meaning the game 
+  ////       is still in progress), render whose turn it is.
+  // //    - If `winner` is false, but `tie` is true, render a tie message.
+  ////     - Otherwise, render a congratulatory message to the player that has 
+  ////      won.
 
+  //// 4f) Invoke both the `updateBoard` and the `updateMessage` functions
+  ////     inside of your `render` function.
 
-// Step 6.1 - `placePiece`
+//todo Step 5 - Define the required constants
 
-  // 6.1a) Create a function named placePiece that accepts an `idx` parameter.
-
-  // 6.1b) Update the `board` array at the `idx` so that it is equal to the 
-  //       current value of `turn`.
-
-
-// 6.2 - `checkForTie`
-
-  // 6.2a) Create a function named `checkForTie`.
-
-  // 6.2b) Check if the `board` array still contains any `null` elements. If
-  //       it does, we can leave `tie` as false. Otherwise, set `tie` to true.
+  //// 5a) In a constant called `winningCombos` define the eight possible winning 
+  ////     combinations as an array of arrays.
 
 
-// 6.3 - `checkForWinner`
+//todo Step 6 - Handle a player clicking a square with a `handleClick` function
+
+  //// 6a) Create a function called `handleClick`. It will have an `evt`
+  ////     parameter.
+
+  // //6b) Attach an event listener to the game board (you can do this to each
+  // //   one of the existing `squareEls` with a `forEach` loop OR add a new
+  // //    cached element reference that will allow you to take advantage of 
+  // //    event bubbling). On the `'click'` event, it should call the 
+  ////    `handleClick` function you created in 6a.
+
+  //// 6c) Obtain the index of the square that was clicked by "extracting" the 
+  ////     index from an `id` assigned to the target element in the HTML. Assign 
+  ////     this to a constant called `sqIdx`.
+
+  //// 6d) If the `board` has a value at the `sqIdx`, immediately `return`  
+  // //    because that square is already taken. Also, if `winner` is not `null`
+  //   //  immediately `return` because the game is over.
+
+
+//todo Step 6.1 - `placePiece`
+
+  // //6.1a) Create a function named placePiece that accepts an `idx` parameter.
+
+  // //6.1b) Update the `board` array at the `idx` so that it is equal to the 
+  // //      current value of `turn`.
+
+
+//todo 6.2 - `checkForTie`
+
+  // //6.2a) Create a function named `checkForTie`.
+
+  //// 6.2b) Check if the `board` array still contains any `null` elements. If
+  // //      it does, we can leave `tie` as false. Otherwise, set `tie` to true.
+
+
+//todo 6.3 - `checkForWinner`
 
   // 6.3a) Create a function called `checkForWinner`
 
@@ -153,7 +231,7 @@
   //       `winner` to true.
 
 
-// 6.4 - `switchPlayerTurn`
+//todo 6.4 - `switchPlayerTurn`
 
   // 6.4a) Create a function called `switchPlayerTurn`.
 
